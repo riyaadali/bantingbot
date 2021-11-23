4bb4e6bd6ab05f8acf6eb367fbe38502e65a7159
riyaadali <anon@anon.com>
2021-11-23 14:08:30 -0500

    Modified soccer.py to retrieve current date, compare against soccer schedule.

--- soccer.py
+++ soccer.py
@@ -9,10 +9,8 @@ import time
 
 t = time.localtime()
 month_day = time.strftime('%b %d', t)
-
-month_day = "Nov 27" # For testing
-
 soccerURL = "https://registration.sportsavvy.com/OVL/index.php"
+field = ""
 
 # POST data for soccer site
 soccerPOST = {'YKey': '633',
@@ -30,7 +28,8 @@ for tr in soup.find_all('tr')[2:]:
     if month_day in str(tds[0]):
        field = str(tds[6]).replace("<td>PGI-","").replace("</td>","")          
           
-slacker = Slacker()
-slacker.sendMessage("Your son's soccer field - %s" % field, "Today your son will be playing on field *%s*" % field)
+if field:
+  slacker = Slacker()
+  slacker.sendMessage("Your son's soccer field - %s" % field, "Today your son will be playing on field *%s*" % field)
 
           
